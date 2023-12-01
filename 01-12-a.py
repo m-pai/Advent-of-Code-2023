fd3a6a06ea729c33733ed5366e4271d6e38216b2
thanksf0rallthefish <99610260+m-pai@users.noreply.github.com>
2023-12-01 17:54:55 +0530

    comments to explain code

--- 01-12-a.py
+++ 01-12-a.py
@@ -1,9 +1,11 @@
 # advent of code 2023 day 1 question 1
 
+#function to convert a string to a number
 def strnum(i):
     a = ''.join(c for c in i if c.isdigit())
     return a
 
+#function to take the first and last numbers of a string and convert it into an integer
 def strtonum(a):
     c = a[0] + a[-1]
     return int(c)
